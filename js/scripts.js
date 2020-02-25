68ac51d72abaee7cf18d9ae29334d24c97d1850f
Anatoli Railean <arailean@eagles.ewu.edu>
2020-02-24 22:30:46 -0800

    gsap animation

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,14 @@ console.log('OK');
 let stageHeight = document.getElementById('stage').offsetHeight;
 let stageWidth = document.getElementById('stage').offsetWidth;
 
-document.getElementById(`cosmonaut`).style.position = "absolute";
-document.getElementById(`cosmonaut`).style.top = `${stageHeight-70}px`;
-document.getElementById(`cosmonaut`).style.right = `${stageWidth-60}px`;
+// document.getElementById(`cosmonaut`).style.position = "absolute";
+// document.getElementById(`cosmonaut`).style.top = `${stageHeight-70}px`;
+// document.getElementById(`cosmonaut`).style.right = `${stageWidth-60}px`;
 
 document.getElementById(`rocket`).style.position = "absolute";
 document.getElementById(`rocket`).style.top = `${stageHeight-60}px`;
 document.getElementById(`rocket`).style.right = `${stageWidth-110}px`;
+document.getElementById(`rocket`).style.zIndex = "2";
 
 const positionStars = () => {
     let i;
@@ -23,7 +24,6 @@ const positionStars = () => {
         document.getElementById(`star-${i}`).style.position = "absolute";
         document.getElementById(`star-${i}`).style.top = `${starYRandomPosition}px`;
         document.getElementById(`star-${i}`).style.right = `${starXRandomPosition}px`;
-
     }
 };
 
@@ -33,20 +33,28 @@ const interval = setInterval(function() {
     positionStars();
 }, 5000);
 
-gsap.from("#cosmonaut", {
+let timeLineRocket = gsap.timeline();
+
+timeLineRocket.from("#cosmonaut", {
     opacity: 0,
-    y: -200,
+    y: -10,
     duration: 1
 });
 
-gsap.to("#cosmonaut", 2, {
+timeLineRocket.to("#rocket", 2, {
     x: 100,
     y: -200
 })
 
-gsap.to("#rocket", 2, {
+timeLineRocket.to("#rocket", 2, {
+    x: 450,
+    y: -200,
+    rotationY: 180
+})
+
+timeLineRocket.to(["#rocket", "#cosmonaut"], 2, {
     x: 100,
-    y: -200
+    y: -220
 })
 
 document.addEventListener("mouseover", () => {
